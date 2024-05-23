20ca6913b9163ee7ed83ea3e64bbf3790c1c4f69
Jackson Mena <mena.jack72@gmail.com>
2024-05-22 23:42:36 -0300

    create user token

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,3 +1,4 @@
+const createUserToken = require("../helpers/create-user-token");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
@@ -53,7 +54,7 @@ module.exports = class UserController {
 
     try {
       const newUser = await user.save();
-      res.status(201).json({ message: "Usuário criado!", newUser });
+      await createUserToken(newUser, req, res);
     } catch (error) {
       res.status(500).json({ message: error });
     }
