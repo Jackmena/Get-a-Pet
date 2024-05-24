a7fe5ea3d066e19d5264415098974b955167e5c0
Jackson Mena <mena.jack72@gmail.com>
2024-05-24 20:18:43 -0300

    add function login

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const UserController = require("../controllers/UserControllers");
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 module.exports = router;
