30ccb98b51b83cd99bb68e5bd2ce2a05fa6adfaa
Jackson Mena <mena.jack72@gmail.com>
2024-06-06 22:23:54 -0300

    pet images upload

--- backend/controllers/PetControllers.js
+++ backend/controllers/PetControllers.js
@@ -6,6 +6,8 @@ module.exports = class PetController {
   static async create(req, res) {
     const { name, age, weight, color } = req.body;
 
+    const images = req.files;
+
     const available = true;
 
     // images upload
@@ -27,6 +29,10 @@ module.exports = class PetController {
       res.status(422).json({ message: "A cor é obrigatória!" });
       return;
     }
+    if (images.length === 0) {
+      res.status(422).json({ message: "A imagem é obrigatória!" });
+      return;
+    }
 
     //get pet owner
     const token = getToken(req);
@@ -46,6 +52,10 @@ module.exports = class PetController {
         image: user.image,
         phone: user.phone,
       },
+    });
+
+    images.map((image) => {
+      pet.images.push(image.filename);
     });
 
     try {
